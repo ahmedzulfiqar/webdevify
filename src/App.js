0e8fe14aa27deab89c60cdc55c60941da4ec57f0
ahmedzulfiqar <ahmdzulfiqar01@example.com>
2023-09-07 14:18:41 +0500

    ds

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Routercomp from "./router/Routercomp";
 import Appcover from "./components/covers/Appcover";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Appcover>
-        <Routercomp />
+        <Home />
       </Appcover>
     </div>
   );
